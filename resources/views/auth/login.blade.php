251a3d06563a3d36372e7691f4772067a96ff334
SnowWhite1049 <viper.it.dev@gmail.com>
2023-06-13 21:15:15 +0900

    login, register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,151 +1,145 @@
-@extends("layouts.auth")
+<!-- @extends("layouts.auth") -->
+<!DOCTYPE html>
+<html lang="ja" prefix="og: http://ogp.me/ns#">
+
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="#ffffff" name="theme-color" />
+    <meta name="csrf_token" content="{{ csrf_token() }}">
+    <title>{{ env('APP_NAME') }}</title>
+    <meta content="on" http-equiv="x-dns-prefetch-control" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/tsukubnobi/tsukunobi_favicon.svg') }}" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+    <script async="" crossorigin="anonymous" src="{{ asset('assets/js/fontawesome.js') }}"></script>
+    <link rel="stylesheet" media="all" href="{{ asset('assets/vendor/css/dashboard-1.css') }}" />
+    <link rel="stylesheet" media="all" href="{{ asset('assets/vendor/css/dashboard-responsive.css') }}" />
+    <link rel="stylesheet" media="all" href="https://precompiled-assets.boxil.jp/assets/application-ad1c2e0774638a9874c7ce9b343d948ebe76d23a8686d7ac92f28ca8d37a87a5.css" />
+    <script src="{{ asset('assets/js/dashboard-index.js') }}"></script>
+    <style>
+        /* .moda-users-title {
+            padding: 10px 0;
+        }
+        .modal-title {
+            color: #FFFFFF;
+            text-align: center;
+            font-size: 22px;
+            font-weight: normal;
+        } */
+        .moda-users-title {
+            margin: 0 auto;
+        }
+        .modal-header.modal-header-color {
+            background-color: #006666;
+        }
+        .modal-header {
+            border-radius: 0 0 0 0 !important;
+            padding: 5px;
+            border-bottom: 1px solid #e5e5e5;
+            min-height: 16.428571429px;
+        }
+        .modal-header-color {
+            padding: 2px 10px !important;
+        }
+        .btn-md, .btn-orange-md, .btn-green-md, .btn-indigo-md, .btn-gray-md, .btn-light-gray-md, .btn-blue-md, .btn-red-md {
+            width: 120px;
+            padding: 15px 0;
+            font-size: 14px;
+        }
+        .modal-users-btn-wrapper .btn-modal-part-signin {
+            width: 100%;
+        }
+        .btn-boxil-orange, .btn-orange-xs, .btn-orange-sm, .btn-orange-md, .btn-orange-lg, .btn-orange-xl, .btn-orange-full, .btn-download-md {
+            background: #F66800 !important;
+        }
+        /* #modal-signup .middle-line:before, #modal-signin .middle-line:before, #modal-signup-request .middle-line:before {
+            content: "";
+            display: block;
+            border-top: solid 1px #969a97;
+            width: 80%;
+            height: 1px;
+            position: absolute;
+            top: 50%;
+            left: 10%;
+            z-index: 1;
+        } */
+    </style>
+
+</head>
 
 @section("content")
 <div class="card auth_card">
 	<div class="card-body">
-		<!-- Logo -->
-		<div class="app-brand justify-content-center">
-			<a href="" class="app-brand-link gap-2">
-				<span class="app-brand-logo demo">
-					<svg
-						width="25"
-						viewBox="0 0 25 42"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-						xmlns:xlink="http://www.w3.org/1999/xlink"
-					>
-						<defs>
-							<path
-								d="M13.7918663,0.358365126 L3.39788168,7.44174259 C0.566865006,9.69408886 -0.379795268,12.4788597 0.557900856,15.7960551 C0.68998853,16.2305145 1.09562888,17.7872135 3.12357076,19.2293357 C3.8146334,19.7207684 5.32369333,20.3834223 7.65075054,21.2172976 L7.59773219,21.2525164 L2.63468769,24.5493413 C0.445452254,26.3002124 0.0884951797,28.5083815 1.56381646,31.1738486 C2.83770406,32.8170431 5.20850219,33.2640127 7.09180128,32.5391577 C8.347334,32.0559211 11.4559176,30.0011079 16.4175519,26.3747182 C18.0338572,24.4997857 18.6973423,22.4544883 18.4080071,20.2388261 C17.963753,17.5346866 16.1776345,15.5799961 13.0496516,14.3747546 L10.9194936,13.4715819 L18.6192054,7.984237 L13.7918663,0.358365126 Z"
-								id="path-1"
-							></path>
-							<path
-								d="M5.47320593,6.00457225 C4.05321814,8.216144 4.36334763,10.0722806 6.40359441,11.5729822 C8.61520715,12.571656 10.0999176,13.2171421 10.8577257,13.5094407 L15.5088241,14.433041 L18.6192054,7.984237 C15.5364148,3.11535317 13.9273018,0.573395879 13.7918663,0.358365126 C13.5790555,0.511491653 10.8061687,2.3935607 5.47320593,6.00457225 Z"
-								id="path-3"
-							></path>
-							<path
-								d="M7.50063644,21.2294429 L12.3234468,23.3159332 C14.1688022,24.7579751 14.397098,26.4880487 13.008334,28.506154 C11.6195701,30.5242593 10.3099883,31.790241 9.07958868,32.3040991 C5.78142938,33.4346997 4.13234973,34 4.13234973,34 C4.13234973,34 2.75489982,33.0538207 2.37032616e-14,31.1614621 C-0.55822714,27.8186216 -0.55822714,26.0572515 -4.05231404e-15,25.8773518 C0.83734071,25.6075023 2.77988457,22.8248993 3.3049379,22.52991 C3.65497346,22.3332504 5.05353963,21.8997614 7.50063644,21.2294429 Z"
-								id="path-4"
-							></path>
-							<path
-								d="M20.6,7.13333333 L25.6,13.8 C26.2627417,14.6836556 26.0836556,15.9372583 25.2,16.6 C24.8538077,16.8596443 24.4327404,17 24,17 L14,17 C12.8954305,17 12,16.1045695 12,15 C12,14.5672596 12.1403557,14.1461923 12.4,13.8 L17.4,7.13333333 C18.0627417,6.24967773 19.3163444,6.07059163 20.2,6.73333333 C20.3516113,6.84704183 20.4862915,6.981722 20.6,7.13333333 Z"
-								id="path-5"
-							></path>
-						</defs>
-						<g id="g-app-brand" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-							<g id="Brand-Logo" transform="translate(-27.000000, -15.000000)">
-								<g id="Icon" transform="translate(27.000000, 15.000000)">
-									<g id="Mask" transform="translate(0.000000, 8.000000)">
-										<mask id="mask-2" fill="white">
-											<use xlink:href="#path-1"></use>
-										</mask>
-										<use fill="#696cff" xlink:href="#path-1"></use>
-										<g id="Path-3" mask="url(#mask-2)">
-											<use fill="#696cff" xlink:href="#path-3"></use>
-											<use fill-opacity="0.2" fill="#FFFFFF" xlink:href="#path-3"></use>
-										</g>
-										<g id="Path-4" mask="url(#mask-2)">
-											<use fill="#696cff" xlink:href="#path-4"></use>
-											<use fill-opacity="0.2" fill="#FFFFFF" xlink:href="#path-4"></use>
-										</g>
-									</g>
-									<g
-										id="Triangle"
-										transform="translate(19.000000, 11.000000) rotate(-300.000000) translate(-19.000000, -11.000000) "
-									>
-										<use fill="#696cff" xlink:href="#path-5"></use>
-										<use fill-opacity="0.2" fill="#FFFFFF" xlink:href="#path-5"></use>
-									</g>
-								</g>
-							</g>
-						</g>
-					</svg>
-				</span>
-				<span class="app-brand-text demo text-body fw-bolder" style="text-transform: uppercase;">{{ env('APP_NAME') }}</span>
-			</a>
-		</div>
-		
-		<!-- /Logo -->
-		<h4 class="mb-3">{{ env('APP_NAME') }}へようこそ！ 👋</h4>
-
-		@if ($errors->any())
-		<div class="alert alert-danger alert-dismissible" role="alert">
-			{{__('messages.auth.email_or_password_error')}}
-			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-		</div>
-			
-		@endif
-		<form id="formAuthentication" class="mb-3" method="POST" action="{{ route('login') }}">
-			@csrf
-			<div class="mb-3">
-				<label for="email" class="form-label">Email</label>
-				<input
-					type="email"
-					class="form-control"
-					id="email"
-					name="email"
-					placeholder="Emailを入力してください。"
-					autofocus
-				/>
-			</div>
-			@error('email')
-				<small class="text-danger text-xs">{{ $message }}</small>
-			@enderror
-			<div class="mb-3 form-password-toggle">
-				<div class="d-flex justify-content-between">
-					<label class="form-label" for="password">{{__('messages.profile.password')}}</label>
-					<!-- <a href="{{ route('forgot') }}">{{__('messages.auth.forget_password')}}</a> -->
-				</div>
-				<div class="input-group input-group-merge">
-					<input
-						type="password"
-						id="password"
-						class="form-control"
-						name="password"
-						placeholder="{{__('messages.profile.password')}}を入力してください。"
-						aria-describedby="password"
-					/>
-					<span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
-				</div>
-				@error('password')
-					<small class="text-danger text-xs">{{ $message }}</small>
-				@enderror
-			</div>
-			<!-- <div class="mb-3">
-				<div class="form-check">
-					<input class="form-check-input" type="checkbox" id="remember-me" />
-					<label class="form-check-label" for="remember-me"> Remember Me </label>
-				</div>
-			</div> -->
-			<div class="mb-3">
-				<button type="submit" class="btn btn-primary d-grid w-100">{{__('messages.auth.login')}}</button>
-			</div>
-		</form>
-
-		<p class="text-center">
-			<!-- <span>New on our platform?</span> -->
-			<a href="{{  url('/register') }}">{{__('messages.auth.have_no_account')}}</a>
-		</p>
+        <div class="movable-wrapper">
+            <div class="movable-signin">
+                <div class="modal-header modal-header-color">
+                    <div class="modal-title moda-users-title">ログイン</div>
+                </div>
+                <div class="modal-users-block">
+                    <div class="authButtonWrapper authButtonWrapper__column">
+                        <div class="authButtonWrapper__block">
+                            <a class="btn btn-raised authButtonWrapper__flat" rel="nofollow" data-method="post" href="/users/auth/google_oauth2/"><i class="fa fa-google authButtonWrapper__googleIcon" aria-hidden="true"></i>Googleでログイン</a>
+                        </div>
+                        <div class="authButtonWrapper__block">
+                            <a class="btn btn-raised authButtonWrapper__flat authButtonWrapper__flat__fb js-facebook-auth-link" rel="nofollow" data-method="post" href="/users/auth/facebook/"><i class="fa fa-facebook authButtonWrapper__fbIcon" aria-hidden="true"></i>Facebookでログイン</a>
+                        </div>
+                    </div>
+                    <div class="m-b-10 text-center text-14 text-gray middle-line" style="padding-top: 15px;"><span>または</span></div>
+                    <form class="new_auth_user" id="modal-signin-form" action="/users/sign_in/" accept-charset="UTF-8" data-remote="true" method="post">
+                        <input type="hidden" name="authenticity_token" id="authenticity_token" value="ltIIq4jfCxXF4uNGZ+6ywHWAiyEyq4PiVNgt4yEZHw/vnRqz3dEYKzpSbXI7Fbcp98HZTb1WW5YWY16T9auUYQ==">
+                        <div class="has-error modal-users-error-text" id="modal-signin-errors">
+                            <div></div>
+                        </div>
+                        <div class="modal-users-input-form" id="modal-signin-email">
+                            <input autofocus="autofocus" class="form-control placeholder-no-fix input-chic" placeholder="メールアドレス" required="required" type="email" name="auth_user[email]" id="auth_user_email">
+                        </div>
+                        <div class="modal-users-input-form" id="modal-signin-password">
+                            <input autocomplete="off" class="form-control placeholder-no-fix input-chic" placeholder="パスワード" required="required" type="password" name="auth_user[password]" id="auth_user_password">
+                        </div>
+                        <div class="modal-users-checkbox-wrapper">
+                            <label>
+                                <input name="auth_user[remember_me]" type="hidden" value="0">
+                                <input type="checkbox" value="1" checked="checked" name="auth_user[remember_me]" id="auth_user_remember_me">
+                                <div class="modal-users-checkbox-text">ログインを記憶</div>
+                            </label>
+                        </div>
+                        <div class="modal-users-btn-wrapper none-bt">
+                            <button name="button" type="submit" class="btn btn-raised btn-orange-md btn-modal-part-signin btn-modal-part-signin-email" data-disable-with="ログイン中...">ログイン</button>
+                        </div>
+                        <div class="modal-part-signin-forget-password">
+                            <a class="text-link" href="/users/password/new/">パスワードを忘れた方はこちら<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                        </div>
+                        <div class="margin-top-30">
+                            <a class="btn-raised btn-border-orange" href="/registration/">新規会員登録はこちら</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
 	</div>
 </div>
 @endsection
 
 @section('script')
 <script>
-	$(document).ready(function() {        
-		$("#pass-show").on('click', function(event) {
-			event.preventDefault();
+	// $(document).ready(function() {        
+	// 	$("#pass-show").on('click', function(event) {
+	// 		event.preventDefault();
 
-			if ($('#password').attr("type") == "text") {
-				$('#password').attr('type', 'password');
-				$('#pass-show').removeClass( "fas fa-unlock" );
-				$('#pass-show').addClass("fas fa-lock"); 
-			} else if ($('#password').attr("type") == "password") {
-				$('#password').attr('type', 'text');
-				$('#pass-show').removeClass( "fas fa-lock" );
-				$('#pass-show').addClass("fas fa-unlock");  
-			}
-		});
-	});
+	// 		if ($('#password').attr("type") == "text") {
+	// 			$('#password').attr('type', 'password');
+	// 			$('#pass-show').removeClass( "fas fa-unlock" );
+	// 			$('#pass-show').addClass("fas fa-lock"); 
+	// 		} else if ($('#password').attr("type") == "password") {
+	// 			$('#password').attr('type', 'text');
+	// 			$('#pass-show').removeClass( "fas fa-lock" );
+	// 			$('#pass-show').addClass("fas fa-unlock");  
+	// 		}
+	// 	});
+	// });
 </script>
 @endsection
