963df25a6eea2f9a0e4b15904d8aabd8b81209bf
SnowWhite1049 <viper.it.dev@gmail.com>
2023-06-29 02:01:35 +0900

    delete nodejs

--- nodejs/app/routes/yahoo_route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const yahoo = require("../controllers/yahoo.controller.js");
-
-router.post("/get_info", yahoo.getInfo);
-
-module.exports = router;
